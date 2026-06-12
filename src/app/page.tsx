9495ebf4ca65fb7dadc372ee17780793ccaf26bc
tgillenh2o <tim@findingathome.com>
2026-06-12 11:40:46 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,11 @@ export default async function DashboardPage() {
   }
 
   const safeSessions: Session[] = sessions ?? [];
+  const {
+  data: { user },
+} = await supabase.auth.getUser();
+
+  .eq("user_id", user?.id)
 
   /* ---------------- LAPS ---------------- */
 
