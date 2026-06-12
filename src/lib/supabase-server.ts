e6e4c539c274b928268af9f8bc5df4ad2b7857b9
tgillenh2o <tim@findingathome.com>
2026-06-12 14:11:44 -0500

    Update supabase-server.ts

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
 export function createSupabaseServer() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
