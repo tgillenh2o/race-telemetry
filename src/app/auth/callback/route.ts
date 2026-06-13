ce78a9a0d704053ec502e232666c47f1f5512b6d
tgillenh2o <tim@findingathome.com>
2026-06-12 19:40:58 -0500

    Create route.ts

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { createSupabaseServer } from "@/lib/supabase-server";
+
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url);
+
+  const code =
+    requestUrl.searchParams.get("code");
+
+  if (code) {
+    const supabase =
+      createSupabaseServer();
+
+    await supabase.auth.exchangeCodeForSession(
+      code
+    );
+  }
+
+  return NextResponse.redirect(
+    requestUrl.origin
+  );
+}
